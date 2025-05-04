4774e534260634e2c834145f775a20f1d24efd81
45tera <45terax@gmail.com>
2025-05-04 23:09:52 +0800

    prefill

---
+++ CS102 Prep/Easy - Longest Common Prefix.java
@@ -0,0 +1,41 @@
+/*
+++++++++++++++++++++++++++++
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+++++++++++++++++++++++++++++
+Breakdown:
+1. 
+++++++++++++++++++++++++++++
+Key Takeaways:
+- toCharArray() usage
+- Reading from the back
+
+ */
+class Solution {
+
+    public String isPalindrome(String[] strs) {
+        e
+        return null;
+    }
+
+}
