e8fc6a5f6187ecc5205ccd910bc0d732150ee426
Tan YM <103948042+45tera@users.noreply.github.com>
2026-01-20 12:24:44 +0800

    add: Implement naive Fibonacci solution for climbing stairs
    Added a naive Fibonacci method for climbing stairs.

--- 202/week 2 - climbing stairs.py
+++ 202/week 2 - climbing stairs.py
@@ -4,4 +4,19 @@ Climbing stairs problem:
 • You can either climb 1 or 2 steps each time.
 • In how many distinct ways can you climb to the top?
 '''
+# First find the base case and also
+# M(n) = M(n-1) + M(n-2), M is distinct ways to climb to the top
 
+# inefficient - naive fibonacci method
+def climbStairs(n): #fib
+    if n == 1 or n==2:
+        return n
+    
+    return climbStairs(n-1) + climbStairs(n-2)
+    
+
+print(climbStairs(4)) # Gives 5
+
+
+# TODO: DP - save the value so it doesnt increase
+    
