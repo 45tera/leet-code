d137ac5fcd298f8858480b1e35c787afe24f17dd
Tan YM <103948042+45tera@users.noreply.github.com>
2026-01-20 11:50:42 +0800

    Update week 2 - flattenList.py

--- 202/week 2 - flattenList.py
+++ 202/week 2 - flattenList.py
@@ -1,5 +1,17 @@
-#
+final_flattened = []
+
+#recursion came after like 10-15 minutes 
 def flatten(lst):
-  
+    
+    for subarr in lst:
+        
+        if (type(subarr) == int):
+            final_flattened.append(subarr)
+            
+        else:
+            flatten(subarr)
+            
+        
+    return final_flattened
 
 print(flatten([1, [2, 3], [4, [5, [6, 7]]], [[[8], 9], [10]]]))
