47535922c807c9ba51cc18aa555e12eafacfdc30
Tan YM <103948042+45tera@users.noreply.github.com>
2026-03-16 12:00:31 +0800

    Enhance rod cutting algorithm with memoization
    Added memoization comparison for subproblem in rod cutting algorithm.

--- 202/week 4 - rod_cutting_iter(given).py
+++ 202/week 4 - rod_cutting_iter(given).py
@@ -1,4 +1,4 @@
-#
+#bottom up approach - generating + memoisation
 rod_price = [0,1,5,8,9,10,17,17,20,24,30]
 
 n = len(rod_price) -1 # -1 addresses the '0' at the start
@@ -8,7 +8,7 @@ first_cut = [0] * (n+1)
 def rod_cutting_iterative():
   for i in range(1, n+1): # i stands for the length of the rod
     for j in range(1, i+1): # j stands for the possible cuts from 1 till i (non-inclusive of i+1)
-      new_price = rod_price[j] + max_price[i-j]
+      new_price = rod_price[j] + max_price[i-j] # memoisation comparison of the sub problem
       if new_price > max_price[i]:
         max_price[i] = new_price
         first_cut[i] = j
