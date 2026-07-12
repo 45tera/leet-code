7918cad489e173c5061436db83d673def8af0137
Tan YM <103948042+45tera@users.noreply.github.com>
2026-07-12 14:43:55 +0800

    Refactor isAnagram method initialization
    Refactor isAnagram method to initialize dictionaries inside the length check. Added comments for clarity.

--- Easy - Valid Annagrams.py
+++ Easy - Valid Annagrams.py
@@ -1,3 +1,21 @@
+"""
+++++++++++++++++++++++++++++
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+++++++++++++++++++++++++++++
+"""
+         
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -5,15 +23,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        dict1 = {}
-        dict2 = {}
+
         if len(s) == len(t):
+            # Put here is important - immediate garbage collection on placing it before the if condition IF the check condition fails
+            dict1 = {}
+            dict2 = {}
             for char in s:
                 dict1[char] = dict1.get(char,0) +1
             for char in t:
                 dict2[char] = dict2.get(char,0) +1
         
-            return dict1 == dict2
+            return dict1 == dict2 # Collections.counter is better.
         return False
 
-        
+
